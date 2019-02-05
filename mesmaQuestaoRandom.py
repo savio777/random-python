e3368c9cf47d3b5a2301ec3d6ba59fa17185a8e9
sávio <saviopf22@gmail.com>
2019-02-05 01:46:18 -0300

    pergunta da questao que foi resolvida

--- mesmaQuestaoRandom.py
+++ mesmaQuestaoRandom.py
@@ -1,6 +1,12 @@
 contPeriodos = 0
 
 ''' 
+    Como não teve o primeiro período a partir do semestre passado, 
+    supondo que todos os outros primeiros períodos sejam formados, 
+    em quantos semestres todos os 5 períodos de ads terão turmas formadas?
+'''
+
+''' 
     False => periodo vazio
     True  => periodo com gente
 '''
